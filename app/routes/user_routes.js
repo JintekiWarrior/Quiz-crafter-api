ed72d89335a1464eb2e5874008ef4c09b5245a3b
JintekiWarrior <jintekiwarrior@gmail.com>
2021-08-24 14:06:02 -0400

    changed bcrypt

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 // security packages
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const crypto = require('crypto')
 const passport = require('passport')
 
