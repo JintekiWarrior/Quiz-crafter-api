fdf31d21f5c1b2298eb26f4328ac93f826d908fd
JintekiWarrior <jintekiwarrior@gmail.com>
2021-08-24 14:28:16 -0400

    add signin authentication

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -27,6 +27,7 @@ const requireToken = passport.authenticate('bearer', { session: false })
 // Sign up route
 router.post('/sign-up', (req, res, next) => {
   Promise.resolve(req.body.credentials)
+    // check for errors
     .then(credentials => {
       if (!credentials ||
           !credentials.password ||
@@ -46,6 +47,42 @@ router.post('/sign-up', (req, res, next) => {
     // create the user
     .then(user => User.create(user))
     .then(user => res.status(201).json({ user: user.toObject() }))
+    .catch(next)
+})
+
+router.post('/sign-in', (req, res, next) => {
+  const pw = req.body.credentials.password
+  let user
+
+  // find user based on the email passed
+  User.findOne({ email: req.body.credentials.email })
+    // if we didn't find the user throw an error
+    .then(record => {
+      if (!record) {
+        throw new BadCredentialsError()
+      }
+      // save the user
+      user = record
+      // returns true if the passwords match
+      return bcrypt.compare(pw, user.hashedPassword)
+    })
+
+    .then(correctPassword => {
+      // if the password matched
+      if (correctPassword) {
+        // create a token
+        const token = crypto.randomBytes(16).toString('hex')
+        user.token = token
+        // save the token
+        return user.save()
+      } else {
+        // otherwise throw an error
+        throw new BadCredentialsError()
+      }
+    })
+    // return status 201 and user
+    .then(user => res.status(201).json({ user: user.toObject() }))
+    // check for errors using middleWare
     .catch(next)
 })
 
