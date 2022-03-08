6e27868a79691bdd39213c9d92a84d1baa0cc915
JintekiWarrior <jintekiwarrior@gmail.com>
2022-03-08 11:58:38 -0500

    Add question update to api - Add route for to update a question - test route

--- app/routes/question_routes.js
+++ app/routes/question_routes.js
@@ -24,6 +24,21 @@ router.post('/questions', requireToken, async (req, res, next) => {
     }
 })
 
+// Question Update Request 
+router.patch('/questions/:id', requireToken, async (req, res, next) => {
+    const questionId = req.params.id
+    const quizId = req.body.question.quizId
+    const questionData = req.body.question
+    try {
+        const quiz = await Quiz.findById(quizId)
+        await quiz.questions.id(questionId).set(questionData)
+        res.sendStatus(204)
+        return quiz.save()
+    } catch (error) {
+        return next(error)
+    }
+})
+
 // Question Delete Request Handler
 router.delete('/questions/:id', requireToken, async (req, res, next) => {
     const questionId = req.params.id 
