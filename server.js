c2382b164e768da5287554141d695e1d560fb343
JintekiWarrior <jintekiwarrior@gmail.com>
2022-04-26 14:35:11 -0400

    Add git config file to allow access from github

--- server.js
+++ server.js
@@ -1,42 +1,44 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 // custom middleware functions
-const errorHandler = require('./app/lib/error_handler')
-const requestLogger = require('./app/lib/request_logger')
-const auth = require('./app/lib/auth')
+const errorHandler = require("./app/lib/error_handler");
+const requestLogger = require("./app/lib/request_logger");
+const auth = require("./app/lib/auth");
+
+const db = require("./config/db");
 
 // custome router
-const userRoutes = require('./app/routes/user_routes')
-const quizRoutes = require('./app/routes/quiz_routes')
-const questionRoutes = require('./app/routes/question_routes')
+const userRoutes = require("./app/routes/user_routes");
+const quizRoutes = require("./app/routes/quiz_routes");
+const questionRoutes = require("./app/routes/question_routes");
 
-const app = express()
-let port = process.env.MONGODB_URI || 8080
+const app = express();
+let port = process.env.PORT || 8080;
 
-mongoose.connect('mongodb://localhost/quiz-crafter-db', {
+mongoose.connect(db, {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true
-})
+  useUnifiedTopology: true,
+});
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(auth)
+app.use(auth);
 
-app.use(requestLogger)
+app.use(requestLogger);
 
-app.use(cors())
+app.use(cors());
 
 // routes middleware
-app.use(userRoutes)
-app.use(quizRoutes)
-app.use(questionRoutes)
+app.use(userRoutes);
+app.use(quizRoutes);
+app.use(questionRoutes);
 
 // Last step in middleware
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`listening on port http://localhost:${port} `)
-})
+  console.log(`listening on port http://localhost:${port} `);
+});
